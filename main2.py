b7b534f3df5508fb2ddcf26b228ed342130bc03e
Mrun9 <117284776+Mrun9@users.noreply.github.com>
2024-06-10 12:21:43 -0400

    Code can scrape the names of just 1 field

--- main2.py
+++ main2.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+ 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
