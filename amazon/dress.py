971ad123fef4aea276ae164b7b5342b8e5ac9660
Mrunal Mohan Vibhute <mvibhute@ND.EDU>
2024-06-14 11:32:46 -0400

    Changes made in tsv file

--- amazon/dress.py
+++ amazon/dress.py
@@ -57,7 +57,7 @@ for product, company in products_companies:
 
 # Write the product names and company names to a CSV file
 with open('amazon/amazon_dress.tsv', 'w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
+    writer = csv.writer(file, delimiter='\t')
     writer.writerow(["Product Name", "Company Name"])
     writer.writerows(products_companies)
 
