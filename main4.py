67636d333a9db9ee1660beee3e139a86b1851c0d
Mrunal Mohan Vibhute <mvibhute@ND.EDU>
2024-06-13 21:09:38 -0400

    Updated code to give product and price together

--- main4.py
+++ main4.py
@@ -14,16 +14,29 @@ driver = webdriver.Chrome(service=serv)
 
 driver.get("https://grubhubdrivershop.com/collections/shop-all")
 
+# Wait for the divs with the product titles to be present
+product_divs = WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "product-card__title"))
+)
+
+# Extract and store the text inside each div
+products = [div.text for div in product_divs]
+
 price_divs = WebDriverWait(driver, 10).until(
     EC.presence_of_all_elements_located((By.CLASS_NAME, "price-item--regular"))
 )
 
 prices = [div.text for div in price_divs]
 
-for p in prices:
-    print(p)
+#combine product names and prices together
 
-print(f"No of product prices found: {len(prices)}")
+final = list(zip(products,prices))
+
+#print them together
+for t, p in final:
+    print(f"Product: {t}, Price: {p}")
+
+print(f"No of product prices found: {len(products)}")
 
 time.sleep(20)
 
